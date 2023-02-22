33b06bb4a9eac8be21f4733be4778e3a2ed59029
pink_candy02 <pink_candy02@naver.com>
2023-02-22 16:07:13 +0900

    typesafe-actions라이브러리 활용

--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -1,21 +1,31 @@
+import { ActionType, createReducer, deprecated } from "typesafe-actions";
+const { createStandardAction } = deprecated;
 //액션타입, 액션생성함수, 리듀서
-//1.액션타입
-//action.type이 string으로 추론되지 않고 'counter/INCREASE'와 같이
-//실제 문자열으로 추론되도록 as const 붙임.
-const INCREASE = "counter/INCREASE" as const;
-const DECREASE = "counter/DECREASE" as const;
 
-//2.액션생성함수 return { type: INCREASE, payload: diff }
-const increase = () => ({ type: INCREASE })
-const decrease = () => ({ type: DECREASE })
+//1.액션타입
+const INCREASE = "counter/INCREASE";
+const DECREASE = "counter/DECREASE";
+
+//2.액션생성함수 
+//액션객체에 type 속성만 있거나 받아온값을 그형태 그대로 전달할때 
+export const increase = createStandardAction(INCREASE)();
+export const decrease = createStandardAction(DECREASE)();
 
 //액션객체에 대한 타입
-type CounterAction = 
-ReturnType<typeof increase> 
-| ReturnType<typeof decrease>
+const actions = { increase, decrease }; //액션 생성함수
+//ActionType을 사용하여 모든 액션 객체들의 타입을 지정
+type CounterAction = ActionType<typeof actions>
+
+//상태에 대한 타입
+type CounterState = { count: number }
+
+//초기상태
+const initialState: CounterState = { count: 0 }
 
 //3.리듀서
-function counter(state,action:CounterAction){
+const counter = createReducer<CounterState,CounterAction>(initialState,{
+    [INCREASE]: state => ({ count: state.count + 1 }),
+    [DECREASE]: state => ({ count: state.count - 1 })
+})
 
-}
 export default counter;
